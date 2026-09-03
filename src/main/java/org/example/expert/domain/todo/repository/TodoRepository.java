a943c4199bffb3019a7f721ce45142c4273e3d0c
taeheon <psw4497@gamil.com>
2026-09-03 16:22:57 +0900

    PQL로 작성된 findByIdWithUser 를 QueryDSL로 변경

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -10,7 +10,7 @@ import org.springframework.data.repository.query.Param;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-public interface TodoRepository extends JpaRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository<Todo, Long> , TodoRepositoryCustom {
 
     @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
@@ -25,10 +25,5 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
             @Param("startDate")LocalDateTime startDate,
             @Param("endDate") LocalDateTime endDate,
             Pageable pageable);
-    @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN t.user " +
-            "WHERE t.id = :todoId")
-    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
-
 
 }
